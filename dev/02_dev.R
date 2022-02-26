0230d46da23638037e2abf4bf07b45c528a01d0e
jpoe <16984557+jpoehl@users.noreply.github.com>
2022-02-26 19:15:19 +0100

    Removes superfluous code and packages, and updates data

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -24,8 +24,6 @@ usethis::use_package("tibble")
 usethis::use_package("plyr")
 usethis::use_package("readr")
 usethis::use_package("shinyvalidate")
-usethis::use_package("bslib")
-usethis::use_package("thematic")
 usethis::use_package("highcharter")
 
 ## Add modules ----
@@ -49,59 +47,8 @@ golem::add_css_file("custom")
 
 ## Add internal datasets ----
 ## If you have data in your package
-usethis::use_data_raw(name = "usrds_esrd_mort_2018", open = FALSE) 
+usethis::use_data_raw(name = "usrds_esrd_mort_2019", open = FALSE) 
 
-## Tests ----
-## Add one line by test you want to create
-usethis::use_test( "app" )
-
-# Documentation
-
-## Vignette ----
-usethis::use_vignette("sloper")
-devtools::build_vignettes()
-
-## Code Coverage----
-## Set the code coverage service ("codecov" or "coveralls")
-usethis::use_coverage()
-
-# Create a summary readme for the testthat subdirectory
-covrpage::covrpage()
-
-## CI ----
-## Use this part of the script if you need to set up a CI
-## service for your application
-## 
-## (You'll need GitHub there)
-usethis::use_github()
-
-# GitHub Actions
-usethis::use_github_action() 
-# Chose one of the three
-# See https://usethis.r-lib.org/reference/use_github_action.html
-usethis::use_github_action_check_release() 
-usethis::use_github_action_check_standard() 
-usethis::use_github_action_check_full() 
-# Add action for PR
-usethis::use_github_action_pr_commands()
-
-# Travis CI
-usethis::use_travis() 
-usethis::use_travis_badge() 
-
-# AppVeyor 
-usethis::use_appveyor() 
-usethis::use_appveyor_badge()
-
-# Circle CI
-usethis::use_circleci()
-usethis::use_circleci_badge()
-
-# Jenkins
-usethis::use_jenkins()
-
-# GitLab CI
-usethis::use_gitlab_ci()
 
 # You're now set! ----
 # go to dev/03_deploy.R
